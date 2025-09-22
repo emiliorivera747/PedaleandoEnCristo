5d889a29091c796bb0d1088936586d7ec6c9f04c
Emilio Rivera <emiliorivera747@gmail.com>
2025-09-21 18:16:15 -0700

    Added footer

--- components/footers/Footer.tsx
+++ components/footers/Footer.tsx
@@ -5,15 +5,16 @@ const Footer = () => {
     <footer className=" text-tertiary-800 py-6 border-tertiary-100 bottom-0 left-0 right-0 text-xs border-t ">
       <div className="h-full text-center items-center  w-full">
         <p className="text-xs text-tertiary-700">
-          &copy; {new Date().getFullYear()} Emilio Ulises Rivera. All rights reserved.
+          &copy; {new Date().getFullYear()} Pedaleando En Cristo. Reservados
+          todos los derechos.
         </p>
         <p className="text-xs mt-2 text-tertiary-700">
           <a href="/privacy" className="hover:text-gray-400">
-            Privacy Policy
+            política de privacidad
           </a>{" "}
           |
           <a href="/terms" className="hover:text-gray-400 ml-2">
-            Terms of Service
+            Términos de servicio
           </a>
         </p>
       </div>
